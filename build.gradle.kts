4aca730735815d2d9990fbe6821b0b59d13d6e33
MJ Fang <fmjsjx@163.com>
2026-04-02 20:34:26 +0800

    [Chore] Bump Spring Boot to 4.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("org.springframework.boot") version "4.0.4"
+    id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
     val kotlinVersion = "2.3.20"
     kotlin("jvm") version kotlinVersion
